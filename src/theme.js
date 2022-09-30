f8e6beca593f2d70635936b1c8b6a4fc2b7bb81c
Naman <sanjeevnamanrana@gmail.com>
2022-09-30 08:51:32 +0530

    Material Ui

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import {createTheme} from "@mui/material";
+export const theme=createTheme({
+    palette:{
+        main:"#1760a5",
+        light:"skyblue"
+    },
+    secondary:{
+        main:"#15c630"
+    },
+    otherColor:{
+        
+    }
+})
